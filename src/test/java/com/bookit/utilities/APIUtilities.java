e98c166f8c835d4837d12f969bfd2c8ec1fe71b5
ozgenkacar <ozkcr@icloud.com>
2020-02-06 09:58:39 -0800

    add step definitions

--- src/test/java/com/bookit/utilities/APIUtilities.java
+++ src/test/java/com/bookit/utilities/APIUtilities.java
@@ -12,63 +12,68 @@ import static io.restassured.RestAssured.*;
 
 public class APIUtilities {
 
-    /**
-     * Method to find duplicates in the list of objects. Override equals method for your custom class and provide strategy of equality.
-     * @param list of objects to search for duplicates
-     * @return true or false
-     */
-    public static boolean hasDuplicates(List list){
-        boolean hasDuplicates = false;
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = 0; j < list.size(); j++) {
-                if (list.get(i).equals(list.get(j)) && i != j) {
-                    hasDuplicates = true;
-                    System.out.println("Duplicate: "+list.get(i));
-                    break;
-                }
-            }
-        }
-        return hasDuplicates;
-    }
+    //  private static String URI = ConfigurationReader.getProperty("bookit.api"+ConfigurationReader.getProperty("environment"));
+    private static String URI =Environment.BASE_URI;
     /**
      * Method that generates access token
+     *
      * @return bearer token
      */
-    public static String getTokenForBookit(){
+    public static String getToken() {
         Response response = given().
                 queryParam("email", ConfigurationReader.getProperty("team.leader.email")).
                 queryParam("password", ConfigurationReader.getProperty("team.leader.password")).
                 when().
                 get("/sign").prettyPeek();
-        return  response.jsonPath().getString("accessToken");
+        return response.jsonPath().getString("accessToken");
     }
 
-  /**
-   * Method that generates access token
- * @param role - type of user. allowed types: student team leader, student team member and teacher
- * @return bearer token
- */
-    public static String getTokenForBookit(String role){
-        String userName="";
-        String password="";
-        if(role.toLowerCase().contains("lead")){
-            userName=ConfigurationReader.getProperty("team.leader.email");
-            password=ConfigurationReader.getProperty("team.leader.password");
-        }else if (role.toLowerCase().contains("teacher")){
-            userName=ConfigurationReader.getProperty("teacher.email");
-            password=ConfigurationReader.getProperty("teacher.password");
-        }else if (role.toLowerCase().contains("member")){
-            userName=ConfigurationReader.getProperty("team.member.email");
-            password=ConfigurationReader.getProperty("team.member.password");
-        }else{
-            throw  new RuntimeException("Invalid user type!");
+    /**
+     * Method that generates access token
+     * @param role - type of user. allowed types: student team leader, student team member and teacher
+     * @return bearer token
+     */
+    public static String getToken(String role) {
+        String userName = "";
+        String password = "";
+        if (role.toLowerCase().contains("lead")) {
+            userName = Environment.LEADER_USERNAME;
+            password = Environment.LEADER_PASSWORD;
+        } else if (role.toLowerCase().contains("teacher")) {
+            userName = Environment.TEACHER_USERNAME;
+            password = Environment.TEACHER_PASSWORD;
+        } else if (role.toLowerCase().contains("member")) {
+            userName = Environment.MEMBER_USERNAME;
+            password = Environment.MEMBER_PASSWORD;
+        } else {
+            throw new RuntimeException("Invalid user type!");
         }
-
         Response response = given().
                 queryParam("email", userName).
                 queryParam("password", password).
                 when().
                 get("/sign").prettyPeek();
-        return  response.jsonPath().getString("accessToken");
+        return response.jsonPath().getString("accessToken");
+    }
+
+
+    /**
+     * Method to find duplicates in the list of objects. Override equals method for your custom class and provide strategy of equality.
+     *
+     * @param list of objects to search for duplicates
+     * @return true or false
+     */
+    public static boolean hasDuplicates(List list) {
+        boolean hasDuplicates = false;
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.size(); j++) {
+                if (list.get(i).equals(list.get(j)) && i != j) {
+                    hasDuplicates = true;
+                    System.out.println("Duplicate: " + list.get(i));
+                    break;
+                }
+            }
+        }
+        return hasDuplicates;
     }
 }
--- src/test/java/com/bookit/utilities/ConfigurationReader.java
+++ src/test/java/com/bookit/utilities/ConfigurationReader.java
@@ -5,30 +5,20 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigurationReader {
-    // this class will be responsible for loading properties file and will provide
-    // access to values based on key names
-    //we use this class toload custom.properties files
-    private static Properties configFile;
 
+    private static Properties configFile;
     static {
         try {
-            //provides access to file
-            //try-catch block stands for handling exceptions
-            //if exception occures, code inside a catch block willbe executed
-            //any class that is related to InputOutput produce checked exceptions
-            //without handling checked exception, you can not run a code
             FileInputStream fileInputStream = new FileInputStream("configuration.properties");
-//initialize properties object
-                  configFile = new Properties();
-            //load configuration.properties file
-                 configFile.load(fileInputStream);
-                 fileInputStream.close();
+            configFile = new Properties();
+            configFile.load(fileInputStream);
+            fileInputStream.close();
         } catch (IOException e) {
-            System.out.println("File was not loaded:(");
+            System.out.println("Failed to load properties file!");
             e.printStackTrace();
         }
     }
-    public static String getProperty(String key){
+    public static String getProperty(String key) {
         return configFile.getProperty(key);
     }
 }
