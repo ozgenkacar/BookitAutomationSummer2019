a183069dc49045ec9a978c35428df4231c1a5796
ozgenkacar <ozkcr@icloud.com>
2020-02-07 09:18:09 -0800

    add step definitions

--- src/test/java/com/bookit/runners/CucumberRunner.java
+++ src/test/java/com/bookit/runners/CucumberRunner.java
@@ -8,10 +8,13 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json"
+        },
         features = "src/test/resources/features",
         glue = "com/bookit/step_definitions",
-        dryRun = true,
-        tags = "@api"
+        dryRun = false,
+        tags ="@print_room_names"
 )
 public class CucumberRunner {
 }
